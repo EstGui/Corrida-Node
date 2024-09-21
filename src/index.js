8683fe0be2da4984722c0bf72300b0c63fd8c985
Guilherme Esteves <guilherme.o.esteves@gmail.com>
2024-09-21 00:15:57 -0300

    Adição de personagens

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const PLAYER1 = {
+    nome: "Mario",
+    velocidade: 4,
+    manobrabilidade: 3,
+    poder: 3,
+    pontos: 0,
+};
+
+const PLAYER2 = {
+    nome: "Bowser",
+    velocidade: 5,
+    manobrabilidade: 2,
+    poder: 5,
+    pontos: 0,
+};
+
+// const PLAYER3 = {
+//     nome: "Luigi",
+//     velocidade: 3,
+//     manobrabilidade: 4,
+//     poder: 4,
+//     pontos: 0,
+// };
+
+// const PLAYER4 = {
+//     nome: "Peach",
+//     velocidade: 3,
+//     manobrabilidade: 4,
+//     poder: 2,
+//     pontos: 0,
+// };
+
+// const PLAYER5 = {
+//     nome: "Yoshi",
+//     velocidade: 2,
+//     manobrabilidade: 4,
+//     poder: 3,
+//     pontos: 0,
+// };
+
+// const PLAYER6 = {
+//     nome: "Donkey",
+//     velocidade: 2,
+//     manobrabilidade: 2,
+//     poder: 5,
+//     pontos: 0,
+// };
+
